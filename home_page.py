57f458946cc3e23fa26b8d26ade15443d689b945
rookiejj9 <rookiejj9@gmail.com>
2024-06-24 14:52:30 +0800

    介绍视频播放关闭，并增加了用js脚本执行滑动到对应element位置，并点击的

--- home_page.py
+++ home_page.py
@@ -1,6 +1,6 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
 
 
 def login_and_register(login_a_classname, register_a_classname):
@@ -63,7 +63,7 @@ def home_page_first_proxy_button(div_classname):
     :param div_classname: 第一个代理按钮div元素classname
     :return: None
     """
-    div_a_list = driver.find_element(By.CLASS_NAME,f'{div_classname}').find_elements(By.TAG_NAME, 'a')
+    div_a_list = driver.find_element(By.CLASS_NAME, f'{div_classname}').find_elements(By.TAG_NAME, 'a')
     for _ in div_a_list:
         a_element = _.get_attribute('href')
         _.click()
@@ -73,6 +73,34 @@ def home_page_first_proxy_button(div_classname):
         assert (a_element == a_element_click_url)
         time.sleep(2)
         driver.back()
+
+
+def video_play2(video2_id):
+    """
+    :param video2_id: 视频播放按钮classname
+    :return: None
+    """
+    # 创建播放按钮和video元素实例
+    video_element = driver.find_element(By.ID, f'{video2_id}')
+    driver.execute_script("arguments[0].scrollIntoView();", video_element)
+
+    # 验证视频链接
+    video_link = video_element.find_element(By.TAG_NAME, "source").get_attribute("src")
+    assert (video_link == 'https://www.360proxy.com/360Proxywebsite.mp4')
+
+    # js执行滑动到对应element位置，并点击
+    driver.execute_script("arguments[0].scrollIntoView(true);", video_element)
+    driver.execute_script("arguments[0].click();", video_element)
+    time.sleep(5)
+    video_watching_play = driver.execute_script("return arguments[0].paused;", video_element)
+    if not video_watching_play:
+        assert True
+    else:
+        assert False
+
+    # 关闭视频播放
+    video_close = driver.find_element(By.CLASS_NAME, "video_mark")
+    driver.execute_script("arguments[0].style.display='none';", video_close)
 
 
 if __name__ == '__main__':
@@ -90,10 +118,12 @@ if __name__ == '__main__':
     driver.find_element(By.ID, 'CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll').click()
 
     # 验证登录连接和注册连接
-    login_and_register('vh-h-btn', 'vh-h-btn.blue')
+    # login_and_register('vh-h-btn', 'vh-h-btn.blue')
     # 按钮点击验证
-    try_now_click('ix-banner-user')
+    # try_now_click('ix-banner-user')
     # 首页视频播放
-    video_play1('video_logo_play')
+    # video_play1('video_logo_play')
     # 首页底部代理按钮连接循环验证
-    home_page_first_proxy_button('ix-banner-proxy')
+    # home_page_first_proxy_button('ix-banner-proxy')
+    # 介绍视频按钮
+    video_play2('intruduct-video')
